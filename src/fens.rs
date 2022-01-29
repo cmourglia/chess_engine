8f1c796949d74614bdcce33b2d2b5707306ebb21
Charly Mourglia <charly.mourglia@gmail.com>
2022-01-29 17:09:03 +0100

    Move fens to their own file

---
+++ src/fens.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)]
+pub const EMPTY_BOARD_FEN: &str = "8/8/8/8/8/8/8/8 w - - -";
+pub const STARTING_BOARD_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+pub const SCOTCH_GAME_FEN: &str =
+    "r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3";
+pub const EPAULETTE_MATE_FEN: &str = "5rkr/8/8/8/8/8/8/1Q4K1 w - - 0 1";
+pub const BACKRANK_MATE_FEN: &str = "6k1/5ppp/8/8/8/8/8/1rk5 w - - 0 1";
+pub const LADDER_MATE_FEN: &str = "6k1/R7/2R5/8/8/8/8/8 w - - 0 1";
+pub const CASTLE_MATE_FEN: &str = "8/8/8/8/8/8/R7/R3K1k1 w Q - 0 1";
+pub const PILLSBURY_MATE_FEN: &str = "5rk1/5p1p/5p1B/8/8/8/8/K6R w - - 0 1";
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,9 @@ mod attacks;
 mod bitboard;
 mod board;
 mod codegen;
+mod fens;
 mod rand;
 mod squares;
-
-const EMPTY_BOARD_FEN: &str = "8/8/8/8/8/8/8/8 w - - -";
-const STARTING_BOARD_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-const SCOTCH_GAME_FEN: &str = "r1bqkbnr/pppp1ppp/2n5/4p3/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq d3 0 3";
 
 fn main() {
     //find_magic_number(e4, relevant_bits, is_bishop)
